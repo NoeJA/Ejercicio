104c8a78f33ba292ea9663299cf0aed2b2329c2b
NoeJA <71842752+NoeJA@users.noreply.github.com>
2020-11-16 17:19:14 -0600

    Posible formato final

---
+++ inventario.cpp
@@ -0,0 +1,65 @@
+#include "inventario.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Agregar artículos
+vector<string> inventario::AddArticle(vector<string> articleVec,string article){
+    articleVec.push_back(article);
+    return(articleVec);
+}
+
+//Borrar artículos
+vector<string> inventario::DelArticle(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell, int index){
+	articleVec.erase(articleVec.begin() + index );
+	nArticles.erase(nArticles.begin() + index );
+	return (//Todos los vectores//);
+
+
+}
+
+//Establece el numero de articulos para cada articlo
+vector<int> inventario::setAmount(vector<int> nArticles,int amount){
+    nArticles.push_back(amount);
+    return(nArticles);
+}
+
+//Establece el costo de compra
+vector<float> inventario::setCost(vector<float> Cost,float cost){
+
+    Cost.push_back(cost);
+    return(Cost);
+}
+//Establece el costo de venta
+vector<float> inventario::setSell(vector<float> Sell,float cost_sell ){
+
+    Sell.push_back(cost_sell);
+    return(Sell);
+}
+
+
+//Tenemos los 4 vectores: articulo, cantidad, costo y  precio de venta
+void inventario::printInv(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell){
+    //Imprime los encabezados
+    cout<<"|"<<"Articulos"<<"\t"<<"No. Articulos"<<"\t"<<"Costo compra"<<"\t"<<"Costo venta"<<"\t"<<"|"<<endl;
+    //Imprime una linea divisoria
+    cout<<"------------------------------------------------------------------"<<endl;
+    //Imprime los elementos de los cuatro vectores de datos, en orden descendente para aparentar una tabla
+    for (int i=0; i<articleVec.size();i++ ){
+        cout<<"|"<<i<<"\t"<<articleVec[i]<<"\t"<<nArticles[i]<<"\t"<<Cost[i]<<"\t"<<Sell[i]<<"\t"<<"|"<<endl;
+    }
+}
+
+//Calcula ganancia con base en los costos de compra y venta
+float inventario::calcGain(vector<int> nArticles,vector<float> Cost,vector<float> Sell){
+    float sumCost=0;
+    float sumVent=0;
+    for (int i=0;i<nArticles.size();i++){
+        sumCost+=Cost[i]*nArticles[i];
+        sumVent+=Sell[i]*nArticles[i];
+    }
+    return(sumVent-sumCost);
+}
---
+++ pru.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+#include "inventario.h"
+using namespace std;
+
+//Declaraci?n de variables
+vector<string> articleVec;
+vector<int> nArticles;
+vector<float> Cost;
+vector<float> Sell;
+
+// Declaraci?n de funciones
+void menu();
+int option_menu();
+
+int main() {
+    //Creamos el objeto minventario de la clase inventario para llamar funciones
+    inventario minventario;
+    menu();
+    
+    int res=1;
+    
+    while (res==1){
+    	menu();
+
+    switch (option_menu()) {
+        case 1: {
+            int num;
+            cout << "Si desea añadir otro articulo presione 1, para salir presione cualquier otro numero" << endl;
+            cin >> num;
+            while (num == 1) {
+                string answer;
+                int amount;
+                float price, costsell;
+                cout << "Ingrese el articulo" << endl;
+                cin >> answer;
+                cout << "Ingrese la cantidad" << endl;
+                cin >> amount;
+                cout << "Ingrese el precio de provedor del articulo"<<endl;
+                cin >> price;
+                cout << "Ingrese el precio de venta del articulo" << endl;
+                cin >> costsell;
+                minventario.AddArticle(articleVec, answer);
+                minventario.setAmount(nArticles, amount);
+                minventario.setCost(Cost, price);
+                minventario.setSell(Sell, costsell);
+                cout<<"Si desea agregar otro articulo ingrese 1, de lo contrario presione otro numero"<<endl;
+                cin>>num;
+            }
+            break;
+        }
+        case 2: {
+            minventario.printInv(articleVec, nArticles, Cost, Sell);
+            break;
+        }
+        case 3: {
+            float gain = minventario.calcGain(nArticles, Cost, Sell);
+            cout << "La ganancia aproximada con base en el inventario es: " << gain << endl;
+            break;
+            
+        case 4:{
+        	int index;
+        	cout<<"Ingrese el indice de elemento a eliminar"<<endl;
+        	cin>>index;
+        	DelArticle(articleVec,index);
+			break;
+		}
+        }
+        default:
+            cout << "Operacion no valida" << endl;
+            break;
+    }
+    cout<<"¿Desea continuar con el inventario? presione 1, de lo contrario, presione otro numero"<<endl;
+    cin>>res;
+}
+    return 0;
+}
+void menu(){
+	cout<<"Ingrese la opcion que desea utilizar"<<endl;
+	cout<<"1.Añadir un articulo"<<endl<<"2.Visualizar inventario"<<endl<<"3.Calcular ganancia"<<"4.Eliminar un articulo"<<endl;
+	
+	
+}
+int option_menu(){
+	int response;
+	cout<<"Ingrese la opcion que desea utilizar"<<endl;
+	cin>>response;
+	return(response);
+}
