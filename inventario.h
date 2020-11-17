9bb9f44945036216dba08862859be410ea218769
NoeJA <71842752+NoeJA@users.noreply.github.com>
2020-11-16 21:05:43 -0600

    Update inventario.h

--- inventario.h
+++ inventario.h
@@ -15,8 +15,9 @@ private:
     vector<float> Sell;
 public:
     vector<string> AddArticle(vector<string> articleVec,string article);
-    vector<string> DelArticle(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell, int index);
+    void DelArticles(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell, int index, int quant);
     vector<int> setAmount(vector<int> nArticles,int amount);
+    void DelArticle(vector<int> nArticles, int index, int quant);
     vector<float> setCost(vector<float> Cost,float cost);
     vector<float> setSell(vector<float> Sell,float costsell );
     void printInv(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell);
